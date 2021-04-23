ac75e114ed7ee51b96a4ed57e70161b418425ee0
Heitor Carvalho de Almeida Neto <heitorh3@gmail.com>
2021-04-23 13:27:08 -0300

    Adicionando o componente user

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -24,6 +24,7 @@ const app = express();
 
 app.use(express.json());
 
+
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFile));
 
 Sentry.init({ dsn: sentryConfig.dsn, tracesSampleRate: sentryConfig.tracesSampleRate });
